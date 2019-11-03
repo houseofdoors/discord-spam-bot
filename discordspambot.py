16d0fac88d9e0b59bc7e0ca8e2c9cc3bc5ec9517
houseofdoors <anonymous5909@protonmail.com>
2019-11-03 21:54:22 +0000

    Code updates

--- discordspambot.py
+++ discordspambot.py
@@ -1,4 +1,4 @@
-import pyautogui, sys, time
+import pyautogui, sys, keyboard
 
 # Discord spam bot program. I was too busy asking whether or not I could, I forgot to ask whether I should.
 
@@ -6,19 +6,17 @@ def sendspam():
     # Warning
     print('''This bot spams a message of your choice on Discord.
     It sends the message to whatever page you are on in Discord.
-    It does not stop until you press CTRL+C while in the command window.
+    Stop the program by pressing ESC.
     It only posts 1 message per second due to Discord's 5/5 rule. Any faster and you will get IP banned for API abuse.
     This may get you banned, and your friends now hate you.''')
     message = input('Enter your message here: ')
-    try:
-        while True:
-            # Spams the user's desired message, with a short gap to prevent getting stopped for spam.
-            pyautogui.click(400, 990)
-            pyautogui.typewrite(f'{message}')
-            pyautogui.typewrite('\n')
-            time.sleep(1)
-    except KeyboardInterrupt:
-        # Exits the program if the users clicks CTRL+C
-        sys.exit()
+    while True:
+        # Spams the user's desired message, with a short gap to prevent getting stopped for spam.
+        pyautogui.click(400, 990)
+        pyautogui.typewrite(message)
+        pyautogui.typewrite('\n', 1)
+        # Escape system
+        if keyboard.is_pressed('esc'):
+            sys.exit()
 
 sendspam()
